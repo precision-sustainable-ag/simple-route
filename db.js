d627c7c4e48c72c25fcf3b934476b0633ab238e8
Rick Hitchcock <rickhitchcock@outlook.com>
2026-07-31 11:08:49 -0400

    security; hardcode develop database for temp IP 20.241.231.202

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 import 'dotenv/config';
+import os from 'node:os';
 import pg from 'pg';
 
 const { Pool } = pg;
+
+const database =
+  os.hostname() === '20.241.231.202' ? 'develop_species_selector' : process.env.DB_DATABASE;
 
 export const pool = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database,
   port: process.env.DB_PORT,
   ssl: process.env.DB_SSL !== 'false',
   connectionTimeoutMillis: Number(process.env.DB_CONN_TIMEOUT_MS) || 5000,
